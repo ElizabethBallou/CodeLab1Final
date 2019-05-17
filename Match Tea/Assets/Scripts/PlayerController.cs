f2ee36048ee057e39abc6b0f3a91461ec75a8142
ElizabethBallou <epb267@nyu.edu>
2019-05-17 18:29:15 -0400

    Player grayboxed in

---
+++ Match Tea/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newVelocity = new Vector3();
+        
+        if (Input.GetKey(KeyCode.A)) //move left
+        {
+            newVelocity.x += -speed;
+        }
+
+        if (Input.GetKey(KeyCode.D)) //move right
+        {
+            newVelocity.x += speed;
+        }
+
+        rb.velocity = newVelocity;
+    }
+}
---
+++ Match Tea/Assets/Scripts/TeacupController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeacupController : MonoBehaviour
+{
+    public float startY = 7;
+    public float minY = -7;
+    public float xRange = 7;
+    TeacupPool teacupPool;
+    private SpriteRenderer _spriteRenderer;
+    Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        teacupPool = GameObject.Find("PoolHolder").GetComponent<TeacupPool>(); //get the box pool
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < minY) //if the box has fallen to far
+        {
+            Reset(); //reset the object
+        }
+    }
+
+    public void Reset()
+    {
+        //Reset the position to above the screen and a random x position between the -xRange and xRange
+        transform.position = new Vector3(Random.Range(-xRange, xRange), startY, transform.position.z); //reset the position
+        rb.velocity = Vector3.zero; //reset the velocity
+        _spriteRenderer.sprite = teacupPool.SpriteList[Random.Range(0, teacupPool.SpriteList.Count)];
+        gameObject.SetActive(false); //turn off the object
+        teacupPool.teacupList.Add(gameObject); //add it to the box pool
+    }
+}
