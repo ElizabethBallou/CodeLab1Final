cd3c62fa13915acc79f6d091deddebd7af1f854f
ElizabethBallou <epb267@nyu.edu>
2019-05-17 17:56:43 -0400

    moved towards teacup catching game

---
+++ Match Tea/Assets/Scripts/TeacupPool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeacupPool : MonoBehaviour
+{
+    public List<GameObject> teacupList;
+    private SpriteRenderer _spriteRenderer;
+    public List<Sprite> SpriteList;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public GameObject GetTeacup()
+        {
+            GameObject result = null;
+        
+            if (teacupList.Count > 0) //Do we have any boxes to recycle?
+            {
+                //get a box out of the list and recycle it
+                result = teacupList[0];
+                teacupList.Remove(result);
+                result.SetActive(true);
+            }
+            else  //No?
+            {
+                //make a new box
+                result = Instantiate(Resources.Load<GameObject>("Prefabs/teacuporiginal")); //init prefab from resources
+                _spriteRenderer = result.GetComponent<SpriteRenderer>();
+                _spriteRenderer.sprite = SpriteList[Random.Range(0, SpriteList.Count)];
+
+            }
+
+            return result;
+        }
+}
---
+++ Match Tea/Assets/Scripts/TeacupSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeacupSpawner : MonoBehaviour
+{
+    
+    public TeacupPool teacupPool;
+    public float spawnTime = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime); //call spawn every after spawnTime seconds
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        teacupPool.GetTeacup(); //get a box from the pool
+    }
+}
