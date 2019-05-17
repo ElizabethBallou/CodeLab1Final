78cbc652e7278db09918db21228707fea9179c63
ElizabethBallou <epb267@nyu.edu>
2019-05-17 16:49:00 -0400

    thanks Matt

---
+++ Match Tea/Assets/Scripts/TeacupType.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeacupType : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+    public List<Sprite> SpriteList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = SpriteList[Random.Range(0, SpriteList.Count)];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
